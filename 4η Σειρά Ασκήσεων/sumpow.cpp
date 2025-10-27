ab2b97d1605cf10a566b45845ec4f36aaab855f9
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-10-27 19:32:16 +0200

    Create sumpow.cpp
    Δεν λειτουργεί

---
+++ 4η Σειρά Ασκήσεων/sumpow.cpp
@@ -0,0 +1,69 @@
+#include "pzhelp"
+
+int numOfDigits(int num) {
+    //Oloi exoun estw ena digit
+    int totalDigits = 0;
+    int temp = num;
+
+    while (temp > 0) {
+        totalDigits++;
+        temp /= 10;
+    }
+    
+    return totalDigits;
+}
+
+int sumDigits(int* digits, int size, int power) {
+    int sum = 0;
+    
+    for (int i = 0; i < size; i++) {
+        sum += (int)pow(digits[i], power);
+    }
+    
+    return sum;
+}
+
+int pow10(int num) {
+    int res = 1;
+    
+    while (num > 0) {
+        res *= 10;
+        num--;
+    }
+    
+    return res;
+}
+PROGRAM {
+    const int N = READ_INT();
+    
+    int validDigits = 1;
+    while ((validDigits * pow(9, N) < pow(10, validDigits + 1) - 1) && validDigits < 8) {
+        validDigits++;
+    }
+    WRITELN(pow10(validDigits + 1) - 1);
+    if (N <= 9 && N >= 0) {
+        for (int i = 0; i < pow10(validDigits + 1) - 1; i++) {
+            //Creates an array
+            const int totalDigits = numOfDigits(i);
+            int* digits = NEW(int, totalDigits);
+            int tempI = i;
+            
+            //Pernaei ta digits se enan pinaka
+            for (int x = totalDigits - 1; x >= 0; x--) {
+                //Den mas noiazei h seira twn digits
+                digits[x] = tempI % 10;
+                //Einai akeraia diairesh
+                tempI /= 10;
+            }
+            const int sum = sumDigits(digits, totalDigits, N);
+            if (sum > i) {
+                //Skips some iterations
+                i += 10 -  i % pow10(numOfDigits(i) - 1);
+            } else if (sum == i) {
+                WRITELN(i);
+            }
+            //Deletes the array
+            DELETE(digits);
+        }    
+    } 
+}
