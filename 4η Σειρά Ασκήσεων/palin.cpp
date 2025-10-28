a5547e24b36509b01b9eb54fef0ef56bebf7bfb6
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-10-28 12:27:54 +0200

    Create palin.cpp

---
+++ 4η Σειρά Ασκήσεων/palin.cpp
@@ -0,0 +1,50 @@
+#include "pzhelp"
+
+PROGRAM {
+    const int entries = READ_INT();
+    SKIP_LINE();
+    
+    int palins = 0;
+    
+    for (int i = 0; i < entries; i++) {
+        char letter = getchar();
+        char* word = NEW(char, 20);
+        
+        int length = 0;
+        while(letter != '\n' && length <= 20) {
+            word[length] = letter;
+            letter = getchar();
+            length++;
+        }
+        
+        word[length] = '\n';
+        
+        if (length == 0) {
+            //Synexizei sthn idia grammh
+            WRITELN("empty");
+        } else if (length > 20) {
+            WRITELN("error");
+            SKIP_LINE();
+        } else {
+            bool isPalin = true;
+            
+            for (int x = 0; x < length / 2; x++) {
+                if (word[x] != word[length - 1 - x]) {
+                    isPalin = false;
+                    break;
+                }
+            }
+            
+            if (isPalin) {
+                WRITELN("yes");
+                palins++;
+            } else {
+                WRITELN("no");
+            }
+        }
+        
+        DELETE(word);
+    }
+    
+    WRITELN(FORM(palins * 100.0 / entries, 0, 3));
+}
