e11422542227b273f9feeb286f28ee3ddbf8a093
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-10-28 11:42:34 +0200

    Den leitourgouse to 8

--- 4η Σειρά Ασκήσεων/sumpow.cpp
+++ 4η Σειρά Ασκήσεων/sumpow.cpp
@@ -1,5 +1,8 @@
 #include "pzhelp"
 
+//Pinakas me tis dynameis
+int* powers = NEW(int, 9);
+
 int numOfDigits(int num) {
     //Oloi exoun estw ena digit
     int totalDigits = 0;
@@ -13,11 +16,12 @@ int numOfDigits(int num) {
     return totalDigits;
 }
 
-int sumDigits(int* digits, int size, int power) {
+int sumDigits(int* digits, int size) {
     int sum = 0;
     
     for (int i = 0; i < size; i++) {
-        sum += (int)pow(digits[i], power);
+        //Briskei to psifio kai thn antistoixh dynamh
+        sum += powers[digits[i] - 1];
     }
     
     return sum;
@@ -27,18 +31,37 @@ PROGRAM {
     //Briskei to elaxisto panw orio
     const int N = READ_INT();
     
+    for (int i = 0; i < 9; i++) {
+        powers[i] = (int)pow(i + 1, N);
+    }
+    
     int validDigits = 8;
+
     while (validDigits * pow(9, N) < pow(10, validDigits) - 1) {
         validDigits--;
     }
     
     int upper = (int)pow(10, validDigits) - 1;
     
+    int* digits = NEW(int, 1);
+    int tempDigits = 1;
     if (N <= 9 && N >= 0) {
+        int tempDigits = 1;
         for (int i = 0; i <= upper; i++) {
-            //Creates an array
+
             const int totalDigits = numOfDigits(i);
-            int* digits = NEW(int, totalDigits);
+            
+            if (tempDigits < totalDigits) {
+                tempDigits = totalDigits;
+            
+            //Deletes the previous array    
+            DELETE(digits);
+            //Creates a new one
+            digits = NEW(int, totalDigits);
+            } else if (tempDigits == 0) {
+                digits = NEW(int, 1);
+            }
+            
             int tempI = i;
             
             //Pernaei ta digits se enan pinaka
@@ -47,17 +70,14 @@ PROGRAM {
                 //Einai akeraia diairesh
                 tempI /= 10;
             }
-            const int sum = sumDigits(digits, totalDigits, N);
+            const int sum = sumDigits(digits, totalDigits);
             if (sum > i) {
                 //Skips some iterations
-                int temp1 = i;
                 //Einai + 9 kai oxi 10 gt sto telos janaprosthetei 1
                 i += 9 -  i  % 10;
             } else if (sum == i) {
                 WRITELN(i);
             }
-            //Deletes the array
-            DELETE(digits);
         }    
     } 
 }
