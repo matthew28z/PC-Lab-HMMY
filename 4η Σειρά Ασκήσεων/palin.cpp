c715969b985855a162ffc160da562a420e510dbb
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-10-29 21:51:26 +0200

    Update palin.cpp

--- 4η Σειρά Ασκήσεων/palin.cpp
+++ 4η Σειρά Ασκήσεων/palin.cpp
@@ -1,51 +1,55 @@
-#include "pzhelp"
-
-PROGRAM {
-    const int entries = READ_INT();
-    SKIP_LINE();
-    
-    int palins = 0;
-    
-    for (int i = 0; i < entries; i++) {
-        char letter = getchar();
-        //21 giati krataw kai to \n
-        char* word = NEW(char, 21);
-        
-        int length = 0;
-        while(letter != '\n' && length <= 20) {
-            word[length] = letter;
-            letter = getchar();
-            length++;
-        }
-        
-        word[length] = '\n';
-        
-        if (length == 0) {
-            //Synexizei sthn idia grammh
-            WRITELN("empty");
-        } else if (length > 20) {
-            WRITELN("error");
-            SKIP_LINE();
-        } else {
-            bool isPalin = true;
-            
-            for (int x = 0; x < length / 2; x++) {
-                if (word[x] != word[length - 1 - x]) {
-                    isPalin = false;
-                    break;
-                }
-            }
-            
-            if (isPalin) {
-                WRITELN("yes");
-                palins++;
-            } else {
-                WRITELN("no");
-            }
-        }
-        
-        DELETE(word);
-    }
-    
-    WRITELN(FORM(palins * 100.0 / entries, 0, 3));
-}
+#include "pzhelp"  
+  
+PROGRAM {  
+  const int entries = READ_INT();  
+  SKIP_LINE();  
+  
+  int palins = 0;  
+  
+  for (int i = 0; i < entries; i++) {  
+    char letter = getchar();  
+   // 21 giati krataw kai to \n  
+      char* word = NEW(char, 21);  
+  
+    int length = 0;  
+    while(letter != '\n' && length <= 20) {  
+      word[length] = letter;  
+      letter = getchar();  
+      length++;  
+    }  
+  
+    if (letter != '\n') {  
+      SKIP_LINE();  
+    }  
+  
+  
+    word[length] = '\n';  
+  
+    if (length == 0) {  
+      //Synexizei sthn idia grammh  
+      WRITELN("empty");  
+    } else if (length > 20) {  
+      WRITELN("error");  
+    } else {  
+      bool isPalin = true;  
+  
+      for (int x = 0; x < length / 2; x++) {  
+        if (word[x] != word[length - 1 - x]) {  
+          isPalin = false;  
+          break;  
+        }  
+      }  
+  
+      if (isPalin) {  
+        WRITELN("yes");  
+        palins++;  
+      } else {  
+        WRITELN("no");  
+      }  
+    }  
+  
+    DELETE(word);  
+  }  
+  
+  WRITELN(FORM(palins * 100.0 / entries, 0, 3));  
+}  
