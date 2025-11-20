affb18e1b1b31af7fdfdb055a45e5743ecbb72a7
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-11-20 18:44:15 +0200

    Update crypt2.cpp

--- 1ο Εξάμηνο/progintro/5η Σειρά Ασκήσεων/crypt2.cpp
+++ 1ο Εξάμηνο/progintro/5η Σειρά Ασκήσεων/crypt2.cpp
@@ -1,61 +1,90 @@
-#include "pzhelp"
-
-char before[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                   'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                   'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
-                   'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                   'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                   'W', 'X', 'Y', 'Z'
-                  };
-
-char upperCase(char c) {
-	for (int i = 0; i < 26; i++) {
-		if (c == before[i]) {
-			return before[26 + i];
-		}
-	}
-
-	return '\0';
-}
-
-PROGRAM {
-	char after[52];
-
-	for (int i = 0; i < 26; i++) {
-		after[i] = getchar();
-	}
-
-	for (int i = 26; i < 52; i++) {
-		after[i] = upperCase(after[i - 26]);
-	}
-
-	SKIP_LINE();
-
-	char operation = getchar();
-	SKIP_LINE();
-
-	char letter = getchar();
-
-	while (letter != EOF) {
-		if (operation == 'e') {
-			for (int i = 0; i < 52; i++) {
-				if (letter == before[i]) {
-					letter = after[i];
-					break;
-				}
-			}
-		} else {
-			for (int i = 0; i < 52; i++) {
-				if (letter == after[i]) {
-					letter = before[i];
-					break;
-				}
-			}		    
-		}
-
-		WRITE(letter);
-
-		letter = getchar();
-	}
-}
+#include "pzhelp"  
+  
+char before[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
+                   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',  
+                   'q', 'r', 's', 't', 'u', 'v', 'w', 'x',  
+                   'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',  
+                   'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',  
+                   'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',  
+                   'W', 'X', 'Y', 'Z'  
+                  };  
+  
+char upperCase(char c) {  
+    for (int i = 0; i < 26; i++) {  
+        if (c == before[i]) {  
+            return before[26 + i];  
+        }  
+    }  
+  
+    return '\0';  
+}  
+  
+PROGRAM {  
+    char after[52];  
+      
+    int counter = 0;  
+  
+    bool goodInput = true;  
+  
+    for (int i = 0; i < 26; i++) {  
+        after[i] = getchar();  
+        for (int j = counter; j < i; j++) {  
+            if (after[j] == after[i]) {  
+                goodInput = false;  
+                counter++;  
+                break;  
+            }  
+        }  
+  
+        int track = 0;  
+  
+        if (goodInput) {  
+            for (int j = 0; j < 26; j++) {  
+                if (after[i] != before[j]) {  
+                    track++;  
+                }  
+            }  
+  
+            if (track == 26) {  
+                goodInput = false;  
+            }  
+        }  
+    }  
+  
+    if  (goodInput) {  
+        for (int i = 26; i < 52; i++) {  
+            after[i] = upperCase(after[i - 26]);  
+        }  
+  
+        SKIP_LINE();  
+  
+        char operation = getchar();  
+        SKIP_LINE();  
+  
+        char letter = getchar();  
+  
+        while (letter != EOF) {  
+            if (operation == 'e') {  
+                for (int i = 0; i < 52; i++) {  
+                    if (letter == before[i]) {  
+                        letter = after[i];  
+                        break;  
+                    }  
+                }  
+            } else {  
+                for (int i = 0; i < 52; i++) {  
+                    if (letter == after[i]) {  
+                        letter = before[i];  
+                        break;  
+                    }  
+                }  
+            }  
+  
+            WRITE(letter);  
+  
+            letter = getchar();  
+        }  
+    } else {  
+        WRITELN("error");  
+    }  
+} 
