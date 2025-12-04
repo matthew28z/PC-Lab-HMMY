6136c878ec0d2de79dc7d902981ac0e0e1869a60
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-12-04 21:53:20 +0200

    Create revwords.cpp

---
+++ 1ο Εξάμηνο/progintro/6η Σειρά Ασκήσεων/revwords.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool endLine(string word) {
+    return word.back() == '\n';
+}
+
+int main() {
+    vector<string> words;
+
+    while (!cin.eof()) {
+        string word;
+        cin >> word;
+        char c = getchar();
+
+        if (!word.empty()) {
+            if (c == '\n') {
+                word.push_back(c);
+            }
+
+            words.push_back(word);
+        }
+    }
+
+    vector<vector<string>> lines;
+    lines.push_back({ });
+    int l = 0;
+
+    for (string word : words) {
+        lines[l].push_back(word);
+
+        if (endLine(word)) {
+            lines.push_back({ });
+            l++;
+        }
+    }
+
+    for (vector<string> line : lines) {
+        line.back().erase(remove(line.back().begin(), line.back().end(), '\n'), line.back().end());
+
+        for (int i = line.size() - 1; i > 0; i--) {
+            cout << line[i] << " ";
+        }
+
+        cout << line[0] << endl;
+    }
+
+    return 0;
+}
