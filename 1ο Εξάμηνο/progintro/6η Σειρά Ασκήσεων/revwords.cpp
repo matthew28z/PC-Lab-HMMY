17289b0489c95c5efca9cbf563900724f7ecff9a
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-12-10 19:52:04 +0200

    Update revwords.cpp

--- 1ο Εξάμηνο/progintro/6η Σειρά Ασκήσεων/revwords.cpp
+++ 1ο Εξάμηνο/progintro/6η Σειρά Ασκήσεων/revwords.cpp
@@ -11,6 +11,8 @@ bool endLine(string word) {
 
 int main() {
     vector<string> words;
+    char temp;
+    bool isChar = false;
 
     while (!cin.eof()) {
         string word;
@@ -18,7 +20,23 @@ int main() {
         char c = getchar();
 
         if (!word.empty()) {
+            if (isChar) {
+                isChar = false;
+
+                if (temp != ' ' && temp != '\t')
+                word.insert(0, string(1, temp));
+            }
+
             if (c == '\n') {
+                temp = getchar();
+
+                if (temp == '\n') {
+                    isChar = false;
+                    words.push_back(string(1, '\n'));
+                } else {
+                    isChar = true;
+                }
+
                 word.push_back(c);
             }
 
@@ -33,17 +51,25 @@ int main() {
     for (string word : words) {
         lines[l].push_back(word);
 
-        if (endLine(word)) {
-            lines.push_back({ });
+        if (word[0] == '\n') {
+            lines.push_back({string(1, '\n')});
             l++;
+        } else {
+            if (endLine(word)) {
+                lines.push_back({ });
+                l++;
+            }
         }
+
     }
 
     //Clears the trailing empty vector, because the input ends with '\n'
     lines.pop_back();
 
     for (vector<string> line : lines) {
-        line.back().erase(remove(line.back().begin(), line.back().end(), '\n'), line.back().end());
+        if (line.size() > 1) {
+            line.back().erase(remove(line.back().begin(), line.back().end(), '\n'), line.back().end());
+        }
 
         for (int i = line.size() - 1; i > 0; i--) {
             cout << line[i] << " ";
