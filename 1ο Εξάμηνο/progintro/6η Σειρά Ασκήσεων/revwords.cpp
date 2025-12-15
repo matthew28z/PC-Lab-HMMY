eb50da6a315a584cddae965edf3729c188c0454f
matthew28z <89351112+matthew28z@users.noreply.github.com>
2025-12-15 20:21:45 +0200

    Update revwords.cpp

--- 1ο Εξάμηνο/progintro/6η Σειρά Ασκήσεων/revwords.cpp
+++ 1ο Εξάμηνο/progintro/6η Σειρά Ασκήσεων/revwords.cpp
@@ -1,82 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-
-using namespace std;
-
-bool endLine(string word) {
-    return word.back() == '\n';
-}
 
 int main() {
-    vector<string> words;
-    char temp;
-    bool isChar = false;
+    char c = getchar();
 
-    while (!cin.eof()) {
-        string word;
-        cin >> word;
-        char c = getchar();
+    std::vector<std::string> line;
+    std::string word;
 
-        if (!word.empty()) {
-            if (isChar) {
-                isChar = false;
-
-                if (temp != ' ' && temp != '\t')
-                word.insert(0, string(1, temp));
+    while (c != EOF) {
+        if (c != ' ' && c != '\n' && c != EOF) {
+            word += c;
+        } else if (c == ' ') {
+            if (!word.empty()) {
+                line.push_back(word);
+                word.clear();                
+            }
+        } else {
+            if (!word.empty()) {
+                std::cout << word;
             }
 
-            if (c == '\n') {
-                temp = getchar();
+            if (!line.empty()) {  
+                if (!word.empty()) {
+                    std::cout << " ";
+                    word.clear();
+                }     
 
-                if (temp == '\n') {
-                    isChar = false;
-                    words.push_back(string(1, '\n'));
-                } else {
-                    isChar = true;
+                for (int i = line.size() - 1; i > 0; i--) {
+                    std::cout << line[i] << " ";
                 }
 
-                word.push_back(c);
-            }
+                std::cout << line[0] << std::endl;
 
-            words.push_back(word);
-        }
-    }
-
-    vector<vector<string>> lines;
-    lines.push_back({ });
-    int l = 0;
-
-    for (string word : words) {
-        lines[l].push_back(word);
-
-        if (word[0] == '\n') {
-            lines.push_back({string(1, '\n')});
-            l++;
-        } else {
-            if (endLine(word)) {
-                lines.push_back({ });
-                l++;
+                line.clear();
+            } else {
+                std::cout << std::endl;
             }
         }
 
+        c = getchar();
     }
-
-    //Clears the trailing empty vector, because the input ends with '\n'
-    lines.pop_back();
-
-    for (vector<string> line : lines) {
-        if (line.size() > 1) {
-            line.back().erase(remove(line.back().begin(), line.back().end(), '\n'), line.back().end());
-        }
-
-        for (int i = line.size() - 1; i > 0; i--) {
-            cout << line[i] << " ";
-        }
-
-        cout << line[0] << endl;
-    }
-
-    return 0;
 }
